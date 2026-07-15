3b730c09d5ca4b8e05d6e04c1301969e8894da67
jeevanvishnu <jeevanvishnu18@gmail.com>
2026-07-15 16:40:58 +0530

    refactor: minify and format HTML files and add helper scripts for content generation

---
+++ get_color.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import os
+
+img_path = 'c:/Users/jeeva/OneDrive/Documents/Bebright/image.png'
+# The user uploaded an image. Wait, the image is not in the filesystem, it's just in the chat.
+# But I can see the color. It's a deep navy blue. Let me just use a standard navy blue: #0A1C58
+# Or maybe the user meant the blue from the CTA section banner.
